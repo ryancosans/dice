7cc8fcf8ee15edf09b22dff7ab2147d11e61a4e1
ryan cosans <ryanmcosans@gmail.com>
2018-12-18 16:12:48 +0000

    added no exit option

--- Program.fs
+++ Program.fs
@@ -3,11 +3,17 @@
 open System
 open Dice
 
+let roll = fun _ ->
+  let n = Environment.NewLine
+  printfn "%senter dice in format {numberOfDice}d{diceType} e.g: 1d10" n
+  match rollDice <| Console.ReadLine() with
+      | (rolls, sum) -> printf "you rolled: %A total: %i" rolls sum
+
+let rec repeat func =
+  func()
+  repeat func
+
 [<EntryPoint>]
   let main _ =
-    printfn "enter dice in format {numberOfDice}d{diceType} e.g: 1d10"
-
-    match rollDice <| Console.ReadLine() with
-        | (rolls, sum) -> printfn "you rolled: %A total: %i" rolls sum
-
+    repeat <| roll
     0
