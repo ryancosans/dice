3c16fae37ba90969b0918b04e645c1ae2d3f4159
ryan cosans <ryanmcosans@gmail.com>
2018-12-21 11:41:08 +0000

    made code a little nicer

--- Program.fs
+++ Program.fs
@@ -3,7 +3,7 @@
 open System
 open Dice
 
-let roll = fun _ ->
+let roll _ =
   let n = Environment.NewLine
   printfn "%senter dice in format {numberOfDice}d{diceType} e.g: 1d10" n
   match rollDice <| Console.ReadLine() with
@@ -15,5 +15,5 @@ let rec repeat func =
 
 [<EntryPoint>]
   let main _ =
-    repeat <| roll
+    roll |> repeat
     0
